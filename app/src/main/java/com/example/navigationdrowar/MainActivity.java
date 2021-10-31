7a6c8453c39c134bacf1065ef42019e2e16bcd30
HP <mustakim15-2563@diu.edu.bd>
2021-10-31 19:19:48 +0600

    Navigation

---
+++ app/src/main/java/com/example/navigationdrowar/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.navigationdrowar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        drawerLayout=findViewById(R.id.drawer_ID);
+        NavigationView navigationView=findViewById(R.id.navigationView_ID);
+        navigationView.setNavigationItemSelectedListener(this);
+        toggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()==R.id.home_ID){
+            Intent intent=new Intent(MainActivity.this,MainActivity2.class);
+            startActivity(intent);
+        }
+        return false;
+    }
+}
